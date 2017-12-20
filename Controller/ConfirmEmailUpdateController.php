f35defb9ceaf30b917a51cd5a4c8c0ecd158f86b
Dominik Businger <github@azine-it.ch>
2017-12-20 20:42:52 +0100

    fix for #2667: put email-update-confirmation code into separate controller
    Untagled some configuration dependencies between
    email_confirmation (upon registration) and
    confirm_email_update (when the users changes her email)
    
    Fixed parameter access in Mailer/Mailer.php

---
+++ Controller/ConfirmEmailUpdateController.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\UserBundle\Controller;
+
+use FOS\UserBundle\Event\UserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\User;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use FOS\UserBundle\Services\EmailConfirmation\EmailUpdateConfirmation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Controller managing the confirmation of changed user email.
+ *
+ * @author Dominik Businger <git@azine.me>
+ */
+class ConfirmEmailUpdateController extends AbstractController
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
+     * @var UserManagerInterface
+     */
+    private $userManager;
+
+    /**
+     * @var EmailUpdateConfirmation
+     */
+    private $emailUpdateConfirmation;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param UserManagerInterface     $userManager
+     * @param EmailUpdateConfirmation  $emailUpdateConfirmation
+     * @param TranslatorInterface      $translator
+     */
+    public function __construct(EventDispatcherInterface $eventDispatcher, UserManagerInterface $userManager, EmailUpdateConfirmation $emailUpdateConfirmation, TranslatorInterface $translator)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->userManager = $userManager;
+        $this->emailUpdateConfirmation = $emailUpdateConfirmation;
+        $this->translator = $translator;
+    }
+
+    /**
+     * Confirm user`s email update.
+     *
+     * @param Request $request
+     * @param string  $token
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function confirmEmailUpdateAction(Request $request, $token)
+    {
+        /** @var User $user */
+        $user = $this->userManager->findUserByConfirmationToken($token);
+
+        // If user was not found throw 404 exception
+        if (!$user) {
+            throw $this->createNotFoundException($this->translator->trans('email_update.error.message', array(), 'FOSUserBundle'));
+        }
+
+        // Show invalid token message if the user id found via token does not match the current users id (e.g. anon. or other user)
+        if (!($this->getUser() instanceof UserInterface) || ($user->getId() !== $this->getUser()->getId())) {
+            throw new AccessDeniedException($this->translator->trans('email_update.error.message', array(), 'FOSUserBundle'));
+        }
+
+        $this->emailUpdateConfirmation->setUser($user);
+
+        $newEmail = $this->emailUpdateConfirmation->fetchEncryptedEmailFromConfirmationLink($request->get('target'));
+
+        // Update user email
+        if ($newEmail) {
+            $user->setConfirmationToken($this->emailUpdateConfirmation->getEmailConfirmedToken());
+            $user->setEmail($newEmail);
+        }
+
+        $this->userManager->updateUser($user);
+
+        $event = new UserEvent($user, $request);
+        $this->eventDispatcher->dispatch(FOSUserEvents::EMAIL_UPDATE_SUCCESS, $event);
+
+        return $this->redirect($this->generateUrl('fos_user_profile_show'));
+    }
+}
